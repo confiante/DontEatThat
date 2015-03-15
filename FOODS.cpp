aa5e4aae2a62d70526ef5426b427da295c13d04d
confiante <eddiemambo@gmail.com>
2015-03-14 22:35:06 -0400

    replace files

---
+++ FOODS.cpp
@@ -0,0 +1,9 @@
+//
+//  FOODS.cpp
+//  Don't Eat That
+//
+//  Created by ISMD on 3/14/15.
+//  Copyright (c) 2015 ISMD. All rights reserved.
+//
+
+#include "FOODS.h"
---
+++ FOODS.h
@@ -0,0 +1,14 @@
+//
+//  FOODS.h
+//  Don't Eat That
+//
+//  Created by ISMD on 3/14/15.
+//  Copyright (c) 2015 ISMD. All rights reserved.
+//
+
+#ifndef __Don_t_Eat_That__FOODS__
+#define __Don_t_Eat_That__FOODS__
+
+#include <stdio.h>
+
+#endif /* defined(__Don_t_Eat_That__FOODS__) */
